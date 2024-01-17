6275ce1129ee8e63e35f71f09fbecdc265a0367e
cavit99 <cavit@erginsoy.com>
2024-01-17 17:18:56 +0000

    switched variables to yaml config file, improved security practice, hardened error handling,

--- start-sd.py
+++ start-sd.py
@@ -1,8 +1,14 @@
+# MIT License
+# Copyright (c) 2024 Cavit Erginsoy
+
+
 import boto3
 import logging
 import os
 import sys
 import getpass
+import requests
+import yaml
 from botocore.exceptions import NoCredentialsError, ClientError
 
 # Set up logging
@@ -14,12 +20,26 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)  # This will output to the console
     ]
 )
-# Define variables
-LAUNCH_TEMPLATE_ID = 'lt-0e096fddfc03f20b6'
-REMOTE_PORT_NUMBER = '8188'
-LOCAL_PORT_NUMBER = '8188'
-awsregion = 'eu-north-1'
-awstagvalue = 'sd'
+
+# Load the configuration file
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+# Use the configuration values
+LAUNCH_TEMPLATE_ID = config['template']
+REMOTE_PORT_NUMBER = config['remote_port']
+LOCAL_PORT_NUMBER = config['local_port']
+awsregion = config['region']
+awstagvalue = config['tag_value']
+
+
+logging.info(f"Using Launch Template ID: {LAUNCH_TEMPLATE_ID}")
+logging.info(f"Using Remote Port Number: {REMOTE_PORT_NUMBER}")
+logging.info(f"Using Local Port Number: {LOCAL_PORT_NUMBER}")
+logging.info(f"Using AWS region: {awsregion}")
+logging.info(f"Using AWS tag value: {awstagvalue}")
+
+
 user_data = '''#!/bin/bash
 cd /home/ubuntu/stable-diffusion-webui && git rev-parse --is-inside-work-tree > /dev/null 2>&1 && git pull
 cd /home/ubuntu/kohya_ss && git rev-parse --is-inside-work-tree > /dev/null 2>&1 && git pull
@@ -30,24 +50,50 @@ sudo apt-get update
 nohup fio --filename=/dev/nvme0n1 --rw=read --bs=1M --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize &
 '''
 
-def is_aws_configured() -> bool:
+confirmation = input("Press Enter to continue or type q to exit: ")
+if confirmation:
+    logging.info("Exiting...")
+    sys.exit()
+
+def is_connected():
+    try:
+        requests.get('http://www.google.com', timeout=5)
+        return True
+    except requests.exceptions.RequestException:
+        return False
+
+def is_aws_configured() -> boto3.Session:
     try:
         session = boto3.Session()
-        session.get_credentials()
-        return True
+        credentials = session.get_credentials()
+        if credentials is None:
+            return None
+        return session
     except NoCredentialsError:
-        logging.error("AWS is not configured. Please enter your AWS credentials.\nPlease note the credentials will not be stored. In the future you should configure AWS-CLI instead to ensure that the credentials persist")
-        aws_access_key_id = getpass.getpass("Enter your AWS Access Key ID: ")
-        aws_secret_access_key = getpass.getpass("Enter your AWS Secret Access Key: ")
-        region_name = input("Enter your AWS region (e.g., eu-north-1): ") or 'eu-north-1'
-        
-        # Set environment variables
-        os.environ['AWS_ACCESS_KEY_ID'] = aws_access_key_id
-        os.environ['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
-        os.environ['AWS_DEFAULT_REGION'] = region_name
+        return None
 
-        return True
-    
+
+ 
+def does_launch_template_exist(ec2, launch_template_id: str) -> bool:
+    try:
+        response = ec2.describe_launch_templates(
+            LaunchTemplateIds=[launch_template_id]
+        )
+        return bool(response['LaunchTemplates'])
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == 'InvalidLaunchTemplateId.NotFound':
+            logging.error(f"Launch template {launch_template_id} does not exist: {e}")
+        elif error_code == 'InvalidLaunchTemplateId.Malformed':
+            logging.error(f"The launch template ID {launch_template_id} is malformed: {e}")
+        else:
+            logging.error(f"An unexpected error occurred: {e}")
+        return False
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return False
+
+# Create a new instance
 def run_instance(ec2, launch_template_id: str, user_data: str) -> str:
     logging.info("Creating a new instance...")
     instance = ec2.create_instances(
@@ -69,22 +115,33 @@ def get_instance_id(ec2, name_tag: str) -> str:
         ])
     for instance in instances:
         return instance.id
+    logging.info(f"No instances with tag value '{name_tag}' found.")
     return ""
 
+
+# Start the instance if it's not already running
 def start_instance_if_stopped(ec2, instance_id: str) -> None:
     instance = ec2.Instance(instance_id)
     if instance.state['Name'] != 'running':
         logging.info(f"Starting instance {instance_id}...")
         try:
             instance.start()
-            logging.info(f"Waiting for instance {instance_id} to start...")
-            instance.wait_until_running()
+            delay = 15
+            max_attempts = 20
+            logging.info(f"Waiting for instance {instance_id} to start...\nChecking every {delay} seconds, max {max_attempts} attempts")
+            instance.wait_until_running(WaiterConfig={
+                'Delay': delay,
+                'MaxAttempts': max_attempts
+            })
             logging.info(f"Instance {instance_id} is now running.")
         except ClientError as e:
             if e.response['Error']['Code'] == 'UnauthorizedOperation':
-                raise Exception("You are not authorized to start the instance. Please check your AWS permissions.")
+                logging.error("You are not authorized to start the instance. Please check your AWS permissions.")
+                sys.exit(1)  # Exit the script with an error code
             else:
-                raise Exception("An error occurred while starting the instance.")
+                logging.error(f"An error occurred while starting the instance: {e}")
+                sys.exit(1)  # Exit the script with an error code
+
 
 
 def start_ssm_session(ssm, instance_id: str) -> bool:
@@ -97,22 +154,28 @@ def start_ssm_session(ssm, instance_id: str) -> bool:
         logging.info("SSM agent is correctly configured. Attempting to start session...")
         ssm.start_session(Target=instance_id, DocumentName='AWS-StartPortForwardingSession',
                           Parameters={'portNumber': [REMOTE_PORT_NUMBER], 'localPortNumber': [LOCAL_PORT_NUMBER]})
-        logging.info("Session started successfully.")
         return True
     except ClientError as e:
-        logging.error(f"Failed to start a session with the instance: {e}")
         return False
 
 def main() -> None:
 
-    if not is_aws_configured():
-        logging.error("AWS is not configured. Please configure it and try again.")
+    if not is_connected():
+        logging.error("No internet connection. Please check your connection and try again.")
         return
+    
+    session = is_aws_configured()
+    if session is None:
+        logging.error("AWS is not configured. Please enter your AWS credentials.")
+        return
+    
     logging.info("AWS credentials are configured, proceeding.")
+    ec2 = session.client('ec2', region_name=awsregion)
+    ssm = session.client('ssm', region_name=awsregion)
 
-    session = boto3.Session(region_name=awsregion)
-    ec2 = session.resource('ec2')
-    ssm = session.client('ssm')
+
+    if not does_launch_template_exist(ec2, LAUNCH_TEMPLATE_ID):
+        return
 
     existing_instance_id = get_instance_id(ec2, awstagvalue)
 
@@ -130,7 +193,6 @@ def main() -> None:
     else:
         try:
             instance_id = run_instance(ec2, LAUNCH_TEMPLATE_ID, user_data)
-            logging.info(f"Instance {instance_id} is running.")
         except ClientError as e:
             if 'UnauthorizedOperation' in str(e):
                 logging.error("You do not have the necessary permissions to create instances. Please check your IAM policies.")
